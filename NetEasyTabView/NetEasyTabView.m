52833b20455cafff0f75823fd2de9bd40f59708c
mac <mac@leven.local>
2017-03-22 13:19:09 +0800

    improve

--- NetEasyTabView/NetEasyTabView.m
+++ NetEasyTabView/NetEasyTabView.m
@@ -11,6 +11,10 @@
 #define NetEasyTabItemHeight 34
 #define NetEasyTabItemWidth 65
 #define NetEasyTabItemMargin 4
+
+#define SCREEN_WIDTH     [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT    [UIScreen mainScreen].bounds.size.height
+
 /*
  
  为什么tab移动不够迅速呢？估计是因为mask的问题有知道的童鞋请告知
@@ -18,13 +22,17 @@
 */
 
 
-@interface NetEasyTabView ()
+
+@interface NetEasyTabView ()<CAAnimationDelegate>
 {
     CGPoint _beginPoint;
     CGPoint _lastPoint;
     CGPoint _endPoint;
     BOOL _beginIn;
+    BOOL _isAnimation;
     NSInteger _currentIndex;
+    CGFloat _animationOffset;
+    CGFloat _lastOffset;
 }
 
 @property (nonatomic, strong) UIFont *defaultFont;
@@ -60,7 +68,7 @@
     self.layer.cornerRadius = NetEasyTabItemHeight / 2.0;
     self.layer.borderColor = self.slideColor.CGColor;
     self.layer.borderWidth = 0.7;
-    self.backgroundColor = self.bgColor;
+    self.backgroundColor = self.slideColor;
     for (UIView *v in self.subviews) {
         [v removeFromSuperview];
     }
@@ -69,7 +77,7 @@
         NSAssert([str isKindOfClass:[NSString class]], @"title must be NSString!");
         UILabel *bottomLab = [UILabel new];
         bottomLab.text = str;
-        bottomLab.textColor = self.slideColor;
+        bottomLab.textColor = self.bgColor;
         bottomLab.font = self.defaultFont;
         bottomLab.textAlignment = NSTextAlignmentCenter;
         [self addSubview:bottomLab];
@@ -78,102 +86,136 @@
     self.midlleView = [UIView new];
     self.midlleView.userInteractionEnabled = NO;
     self.midlleView.layer.masksToBounds = YES;
-    self.midlleView.backgroundColor = self.slideColor;
+    self.midlleView.backgroundColor = self.bgColor;
 
     [self addSubview:self.midlleView];
     for (NSString *str in self.titlesArray) {
         UILabel *topLab = [UILabel new];
         topLab.text = str;
-        topLab.textColor = self.bgColor;
+        topLab.textColor = self.slideColor;
         topLab.font = self.defaultFont;
         topLab.textAlignment = NSTextAlignmentCenter;
         [self.midlleView addSubview:topLab];
         topLab.frame = [self setTabFrameWithIndex:[self.titlesArray indexOfObject:str]];
     }
     
-    self.sliderLayer = [CALayer new];
-    self.sliderLayer.masksToBounds = YES;
-    self.sliderLayer.backgroundColor = self.bgColor.CGColor;
-    self.sliderLayer.cornerRadius = NetEasyTabItemHeight / 2;
-    self.midlleView.layer.mask = self.sliderLayer;
 
     self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, (NetEasyTabItemWidth + NetEasyTabItemMargin) * self.titlesArray.count - NetEasyTabItemMargin, NetEasyTabItemHeight);
     self.midlleView.frame = self.bounds;
     self.sliderLayer.frame = CGRectMake(0, 0, NetEasyTabItemWidth, NetEasyTabItemHeight);
     _currentIndex = 0;
-    [self layoutIfNeeded];
-    [self.midlleView layoutIfNeeded];
+    
+    [self sliderMaskLayerWithOffset:0 antimation:NO];
+}
 
+-(void )sliderMaskLayerWithOffset:(CGFloat)offset antimation:(BOOL) animation{
+    
+    
+    CGFloat radius = NetEasyTabItemHeight/2;
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, (NetEasyTabItemWidth +NetEasyTabItemMargin) * self.titlesArray.count - NetEasyTabItemMargin, NetEasyTabItemHeight)];
+    [path appendPath:[UIBezierPath bezierPathWithArcCenter:CGPointMake(NetEasyTabItemWidth - radius+offset, radius) radius:radius startAngle:M_PI/2 endAngle:M_PI/2*3 clockwise:NO]];
+    [path addLineToPoint:CGPointMake(radius+offset, NetEasyTabItemHeight)];
+    [path appendPath:[UIBezierPath bezierPathWithArcCenter:CGPointMake(radius +offset, radius) radius:radius startAngle:M_PI/2*3 endAngle:2.5*M_PI clockwise:NO]];
+    [path addLineToPoint:CGPointMake(NetEasyTabItemWidth - radius +offset, 0)];
+    [path closePath];
+    
+    
+    _lastOffset = offset;
+    CAShapeLayer *shapeLayer = (CAShapeLayer *)self.midlleView.layer.mask;
+    if (!shapeLayer) {
+        shapeLayer = [CAShapeLayer layer];
+        
+    }
+    if (animation) {
+        if (_isAnimation) {
+            return;
+        }
+        self.midlleView.layer.mask = shapeLayer;
+        CABasicAnimation *pathAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
+        pathAnimation.toValue = (id)path.CGPath;
+        pathAnimation.duration = 0.15;
+        pathAnimation.removedOnCompletion = NO;
+        pathAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        pathAnimation.fillMode = kCAFillModeForwards;
+        pathAnimation.delegate = self;
+        _isAnimation = YES;
+        [shapeLayer addAnimation:pathAnimation forKey:@"moveAntimation"];
+        _animationOffset = offset;
+    }else{
+        shapeLayer.path = path.CGPath;
+        self.midlleView.layer.mask = shapeLayer;
+    }
 }
 - (CGRect )setTabFrameWithIndex:(NSInteger)index{
     CGFloat coorDinateX = (NetEasyTabItemWidth + NetEasyTabItemMargin) * index;
     return  CGRectMake(coorDinateX, 0, NetEasyTabItemWidth, NetEasyTabItemHeight);
 }
 
+- (NSInteger)getIndexWithPoint:(CGPoint) point{
+    return  point.x / (NetEasyTabItemWidth + NetEasyTabItemMargin);
+}
+
 - (void)didBeginTouchWithPoint:(CGPoint )point{
     _beginPoint = point;
-    if (CGRectContainsPoint(self.bounds, point) ) {
-        _beginIn = YES;
+    if (!CGRectContainsPoint(self.bounds, point) ) {
+        return ;
     }
-    if (!CGRectContainsPoint(self.sliderLayer.frame, _beginPoint) &&CGRectContainsPoint(self.bounds, point) ) {
-        CGRect frame = self.sliderLayer.frame;
-        CGFloat x = point.x -frame.size.width/2;
-        if (x + NetEasyTabItemWidth > self.frame.size.width) {
-            x = self.frame.size.width - NetEasyTabItemWidth;
-        }
-        [UIView animateWithDuration:0.15 animations:^{
-            self.sliderLayer.frame = CGRectMake(x, 0, frame.size.width, frame.size.height);
-        }];
+    NSInteger touchIndex = [self getIndexWithPoint:point];
+    if (touchIndex != _currentIndex) {
+        CGFloat offset = touchIndex * (NetEasyTabItemWidth + NetEasyTabItemMargin);
+        [self sliderMaskLayerWithOffset: offset antimation:YES];
+    }else{
+        
     }
 }
 
 - (void)disEndTouchWithPoint: (CGPoint )point{
-    _endPoint = point;
-    CGRect frame = self.sliderLayer.frame;
-    CGFloat centerX = frame.origin.x + frame.size.width / 2;
-    NSInteger tmp = centerX / (self.frame.size.width / self.titlesArray.count);
+   
+    NSInteger tmp = point.x / (self.frame.size.width / self.titlesArray.count);
+    if (tmp > self.titlesArray.count -1) {
+        tmp = self.titlesArray.count -1;
+    }else if(tmp <= 0){
+        tmp = 0;
+    }
     CGFloat coorDinateX = tmp * (NetEasyTabItemWidth+NetEasyTabItemMargin);
-    [UIView animateWithDuration:0.15 animations:^{
-        self.sliderLayer.frame = CGRectMake(coorDinateX,0 , NetEasyTabItemWidth, NetEasyTabItemHeight);
-    }completion:^(BOOL finished) {
-        if (_currentIndex != tmp) {
+    [self sliderMaskLayerWithOffset:coorDinateX antimation:YES];
+    if (_currentIndex != tmp) {
             !_handle ? :_handle(tmp,self.titlesArray[tmp]);
-            _currentIndex = tmp;
-        }
-    }];
+    }
+    _currentIndex = tmp;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     CGPoint point = [touches.anyObject locationInView:self];
-    NSLog(@"  begin  ---%@",NSStringFromCGPoint(point));
     [self didBeginTouchWithPoint:point];
-
 }
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     CGPoint point = [touches.anyObject locationInView:self];
-    NSLog(@"  end  ---%@",NSStringFromCGPoint(point));
-
     [self disEndTouchWithPoint:point];
-    
 }
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     CGPoint point = [touches.anyObject locationInView:self];
-    NSLog(@"  move  ---%@",NSStringFromCGPoint(point));
-    CGRect frame = self.sliderLayer.frame;
-    CGFloat x = point.x -frame.size.width/2;
-    if (x + NetEasyTabItemWidth > self.frame.size.width) {
-        x = self.frame.size.width - NetEasyTabItemWidth;
+    CGFloat offset = point.x - _beginPoint.x + (_currentIndex *(NetEasyTabItemWidth + NetEasyTabItemMargin));
+
+    if (offset + NetEasyTabItemWidth>= self.frame.size.width) {
+        offset = self.frame.size.width - NetEasyTabItemWidth;
     }
-    if (x < 0) {
-        x = 0;
+    if (offset <= 0) {
+        offset = 0;
     }
-    self.sliderLayer.frame = CGRectMake(x, 0, frame.size.width, frame.size.height);
+    [self sliderMaskLayerWithOffset:offset antimation:NO];
 
 }
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     CGPoint point = [touches.anyObject locationInView:self];
-    if (CGRectContainsPoint(self.frame, point)) {
-        [self disEndTouchWithPoint:point];
+    [self disEndTouchWithPoint:point];
+}
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
+    if (flag) {
+        _isAnimation = NO;
+        [self sliderMaskLayerWithOffset:_animationOffset antimation:NO];
+        [self.midlleView.layer.mask removeAnimationForKey:@"moveAntimation"];
     }
+    
 }
 @end
