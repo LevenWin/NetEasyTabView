86fbd918e569692c1a9891e53a6c3aea26a63f39
mac <mac@leven.local>
2017-03-22 16:29:29 +0800

    git

--- NetEasyTabView/ViewController.m
+++ NetEasyTabView/ViewController.m
@@ -19,9 +19,10 @@
     NetEasyTabView *tabView =[[NetEasyTabView alloc] initWithFrame:CGRectMake(20, 100, 0, 0) titles:@[@"单曲",@"节目",@"MV"] clickBlock:^(NSInteger index, NSString *title) {
         NSLog(@"%@  %lu",title,index);
     }];
+    
     self.view.backgroundColor = [UIColor orangeColor];
     [self.view addSubview:tabView];
-
+    
 }
 
 
